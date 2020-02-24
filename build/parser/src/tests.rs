bb36910dc105686e17d9ed80d52424808198873a
rahulkande <rahulkande@tamu.edu>
2020-02-24 03:02:10 -0600

    added basic_full_1 test

--- build/parser/src/tests.rs
+++ build/parser/src/tests.rs
@@ -2,6 +2,92 @@ use super::*;
 use std::error::Error;
 
 #[test]
+fn basic_full_1() -> Result<(), Box<dyn Error>> { // Basic test covering full grammer
+    let program = program_parser::program(
+        r#"as principal bob password "lmao" do
+              create principal jack "hammer"
+              change password bob "bits"  // please ignore me
+              set x = "done"
+              set a = []
+              //set b = {p = x, q = "that", r = "has"} //dont know how to handle vectors
+              //set d = b.q
+              //set e = {k = d, l = b.r}
+              append to x with "it"
+              local y = "temp_t"
+              foreach x in y replacewith "temp_i"
+              set delegation all jack read -> bob
+              delete delegation x jack read -> bob
+              default delegator = jack
+              return "SUCCESS"
+       ***"#,
+    )?;
+
+    assert_eq!(
+        program,
+        Program {
+            principal: Principal { ident: Identifier { name: "bob".to_string() } },
+            password: "lmao".to_string(),
+            command: Command::Chain(
+              PrimitiveCommand::CreatePrincipal(CreatePrincipal 
+                                    {principal: Principal {ident: Identifier {name: "jack".to_string()}}, 
+                                     password: "hammer".to_string()}),
+              Box::new(Command::Chain(
+                PrimitiveCommand::ChangePassword(ChangePassword 
+                                    {principal: Principal {ident: Identifier {name: "bob".to_string()}}, 
+                                     password: "bits".to_string()}),
+                Box::new(Command::Chain(
+                  PrimitiveCommand::Assignment(Assignment 
+                                    {variable: Variable::Variable (Identifier {name: "x".to_string()}), 
+                                     expr: Expr::Value(Value::String("done".to_string()))}),
+                  Box::new(Command::Chain(
+                    PrimitiveCommand::Assignment(Assignment 
+                                    {variable: Variable::Variable (Identifier {name: "a".to_string()}), 
+                                     expr: Expr::EmptyList}),
+                    Box::new(Command::Chain(
+                      PrimitiveCommand::Append(Append 
+                                    {variable: Variable::Variable (Identifier {name: "x".to_string()}), 
+                                     expr: Expr::Value(Value::String ("it".to_string()))}),
+                      Box::new(Command::Chain(
+                        PrimitiveCommand::LocalAssignment(Assignment 
+                                    {variable: Variable::Variable (Identifier {name: "y".to_string()}), 
+                                     expr: Expr::Value(Value::String ("temp_t".to_string()))}),
+                        Box::new(Command::Chain(
+                          PrimitiveCommand::ForEach(ForEach 
+                                    {value: Variable::Variable (Identifier {name: "x".to_string()}), 
+                                     list: Variable::Variable (Identifier {name: "y".to_string()}), 
+                                     expr: Expr::Value(Value::String ("temp_i".to_string()))}),
+                          Box::new(Command::Chain(
+                            PrimitiveCommand::SetDelegation(Delegation 
+                                    {target: Target::All, 
+                                     delegator: Principal {ident: Identifier {name: "jack".to_string()}}, 
+                                     right: Right::Read,
+                                     delegated: Principal {ident: Identifier {name: "bob".to_string()}}}), 
+                            Box::new(Command::Chain(
+                              PrimitiveCommand::DeleteDelegation(Delegation 
+                                    {target: Target::Variable (Variable::Variable (Identifier {name: "x".to_string()})), 
+                                     delegator: Principal {ident: Identifier {name: "jack".to_string()}}, 
+                                     right: Right::Read,
+                                     delegated: Principal {ident: Identifier {name: "bob".to_string()}}}), 
+                              Box::new(Command::Chain(
+                                PrimitiveCommand::DefaultDelegator(Principal {ident: Identifier {name: "jack".to_string()}}),
+                                Box::new(Command::Return (Expr::Value (Value::String ("SUCCESS".to_string())))) 
+                              ))
+                            ))
+                          ))
+                        ))
+                      ))
+                    ))
+                  ))
+                ))
+              ))
+            )
+        }
+    );
+
+    Ok(())
+}
+
+#[test]
 fn basic() -> Result<(), Box<dyn Error>> {
     let program = program_parser::program(
         r#"as principal bob password "lmao" do
@@ -23,6 +109,52 @@ fn basic() -> Result<(), Box<dyn Error>> {
     );
 
     Ok(())
+}
+
+#[test]
+fn basic_1() -> Result<(), Box<dyn Error>> {
+    let program = program_parser::program(
+        r#"as principal alice password "alices_password" do
+              return msg
+       ***"#,
+    )?;
+
+    assert_eq!(
+        program,
+        Program {
+            principal: Principal {
+                ident: Identifier {
+                    name: "alice".to_string()
+                }
+            },
+            password: "alices_password".to_string(),
+            command: Command::Return(Expr::Value(Value::Variable(Variable::Variable(Identifier{name: "msg".to_string()}))))
+        }
+    );
+
+    Ok(())
+}
+
+
+//#[test]
+//fn basic_2() {
+//    assert!(program_parser::program(
+//        r#"as principal aliiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiice password "alices_password" do
+//              return msg
+//       ***"#,
+//    ).is_err());}
+
+
+#[test]
+fn fail_1() {
+    assert!(program_parser::program(
+        r#"as principal alice password "alices_password" do
+              return msg
+        ***
+           as principal alice password "alices_password" do
+              return msg
+       ***"#,
+    ).is_err());
 }
 
 #[test]
