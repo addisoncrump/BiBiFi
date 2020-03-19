76fca4b8adcc1f66acaec1217f64ad7afd7db732
gpersyn <gpersyn@tamu.edu>
2020-03-19 15:44:50 -0500

    Fuzzer & Oracle error handeling

--- build/fuzzer/src/fuzzer_cmdline.sh
+++ build/fuzzer/src/fuzzer_cmdline.sh
@@ -10,21 +10,25 @@ fi
 #start oracle
 #screen -dmS oracle sh -c "./src/oracle.sh $1; exec bash"
 
+o_err=0
+dut_err=0
+
 while true; do
   SEED=$(find ./seeds -type f | shuf -n 1)
   radamsa $SEED > input.txt
 
-  o_resp=$(netcat 127.0.0.1 $1 <input.txt )
-  dut_resp=$(netcat 127.0.0.1 $1 <input.txt ) #replace this with dut when it works
-  
+  o_resp=$(netcat 127.0.0.1 $1 <input.txt 2>o_err)
+  dut_resp=$(netcat 127.0.0.1 $1 <input.txt 2>dut_err) #replace this with dut when it works
+
 #  if read -t 5 oracle_line <$oracle_pipe; then
 #    echo "Here 56"
-##    if [[ "$oracle_line" == 'quit' ]]; then
- #     echo "Here 57"
- #     break
- #   fi
- #   echo $oracle_line
- # fi
+#    if [[ "$oracle_line" == 'quit' ]]; then
+#      echo "Here 57"
+#      break
+#    fi
+#    echo $oracle_line
+#  fi
+
 
   #equivalence checking
   if [ -z "$o_resp" ] || [ -z "$dut_resp" ] #check is string is empty
@@ -33,8 +37,8 @@ while true; do
     sleep 5
   elif [ "$o_resp" != "$dut_resp" ] 
   then
+    cp input.txt ./mismatches/mm-`date +s%.%N`.txt
     echo "Mismatch: $o_resp == $dut_resp"
-#   echo "Oracle: $oracle_line"
   elif [ "$o_resp" == "$dut_resp" ] 
   then
     echo "Match: $o_resp == $dut_resp"
@@ -43,7 +47,10 @@ while true; do
   fi
 
   #crash handling
-  if [ $? -gt 127 ]; then
+  if [ $o_err -gt 127 ]; then
+    cp input.txt ./crashes/crash-`date +s%.%N`.txt
+    echo "Crash found!"
+  elif [ $dut_err -gt 127 ]; then
     cp input.txt ./crashes/crash-`date +s%.%N`.txt
     echo "Crash found!"
   fi
--- build/fuzzer/src/oracle.sh
+++ build/fuzzer/src/oracle.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
-pipe=/tmp/oraclepipe
+#pipe=/tmp/oraclepipe
 
-if [[ ! -p $pipe ]]; then
-    echo "Reader not running"
-    exit 1
+#if [[ ! -p $pipe ]]; then
+#    echo "Reader not running"
+#    exit 1
+#fi
+
+#check if tcp port is in use
+port_open=$(ss -tulpn | grep -c $1)
+if [ $port_open -eq 1 ]; then
+  echo "Port already in use"
+  exit
 fi
 
-
-#rm ./logs/oracle_out.txt
-#./oracle $1  &> output | $output > ./logs/oracle_out.txt
-#./oracle $1 &>> ./logs/oracle_out.txt
-./oracle $1 &> $pipe
-echo $pipe
+#./oracle $1 &> $pipe
+while true; do
+	./oracle $1 
+	cp input.txt ./crashes/o_crash-`date +s%.%N`.txt
+  echo "Crash found!"
+  sleep 5
+done
