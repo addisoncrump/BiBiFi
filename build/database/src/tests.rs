c46e3e380f8bfc2a0eb4744410ff01548da1e436
Rahul Kande <rahulkande95@gmail.com>
2020-03-23 15:51:48 -0500

    added test for 'all' delegation variable handling

--- build/database/src/tests.rs
+++ build/database/src/tests.rs
@@ -192,27 +192,129 @@ fn basic_full_1() -> Result<(), Box<dyn Error>> {
     my_database.delegate(&Target::Variable("my_var2".to_string()), &"admin".to_string(), &Right::Append, &"alice".to_string());
     my_database.delegate(&Target::Variable("my_var2".to_string()), &"admin".to_string(), &Right::Delegate, &"alice".to_string());
 
-    ////change the default
+    //change the default
+    my_database.set_default_delegator(&"alice".to_string());
+
+    //change bob's password
+    my_database.change_password(&"bob".to_string(), &hash("bob_new_pass".to_string()) );
+    assert_eq!(
+        my_database.check_pass(&"bob".to_string(),&hash("bob_new_pass".to_string())) &&
+            !my_database.check_pass(&"bob".to_string(),&hash("bob_pass".to_string())),
+        true
+    );
+
+    Ok(())
+}
 
 
-    ////change bob's password
-    //my_database.change_password(&"bob".to_string(), &hash("bob_new_pass".to_string()) )
-    //assert_eq!(
-    //    my_database.check_pass(&"bob".to_string(),&hash("bob_new_pass".to_string())) &&
-    //        !my_database.check_pass(&"bob".to_string(),&hash("bob_pass".to_string())),
-    //    true
-    //);
+#[test]
+// test all basic stuff
+fn basic_full_2() -> Result<(), Box<dyn Error>> {
+    let mut my_database = Database::new(hash("wolla".to_string()));
 
-    ////add principals to database after new default alice has some permissions other than prev default anyone
-    //my_database.create_principal(&"john".to_string(), &hash("john_pass".to_string()) );
-    //// check for correct permissions
-    //assert_eq!(
-    //    my_database.check_right(&Target::Variable("my_var2".to_string()), &Right::Read, &"john".to_string()) &&
-    //        my_database.check_right(&Target::Variable("my_var2".to_string()), &Right::Write, &"john".to_string()) &&
-    //        my_database.check_right(&Target::Variable("my_var2".to_string()), &Right::Append, &"john".to_string()) &&
-    //        my_database.check_right(&Target::Variable("my_var2".to_string()), &Right::Delegate, &"john".to_string()),
-    //    true
-    //);
+    //add principals to database
+    my_database.create_principal(&"bob".to_string(), &hash("".to_string())); // empty string password
+    my_database.create_principal(&"tom".to_string(), &hash("tom_pass".to_string()));
+
+    // lets say, bob created my_var and delegated all permissions to everyone
+    my_database.delegate(
+        &Target::Variable("my_var1".to_string()),
+        &"admin".to_string(),
+        &Right::Read,
+        &"bob".to_string(),
+    );
+    my_database.delegate(
+        &Target::Variable("my_var1".to_string()),
+        &"admin".to_string(),
+        &Right::Write,
+        &"bob".to_string(),
+    );
+    my_database.delegate(
+        &Target::Variable("my_var1".to_string()),
+        &"admin".to_string(),
+        &Right::Append,
+        &"bob".to_string(),
+    );
+    my_database.delegate(
+        &Target::Variable("my_var1".to_string()),
+        &"admin".to_string(),
+        &Right::Delegate,
+        &"bob".to_string(),
+    );
+
+    my_database.delegate(
+        &Target::Variable("my_var1".to_string()),
+        &"bob".to_string(),
+        &Right::Read,
+        &"anyone".to_string(),
+    );
+    my_database.delegate(
+        &Target::Variable("my_var1".to_string()),
+        &"bob".to_string(),
+        &Right::Write,
+        &"anyone".to_string(),
+    );
+    my_database.delegate(
+        &Target::Variable("my_var1".to_string()),
+        &"bob".to_string(),
+        &Right::Append,
+        &"anyone".to_string(),
+    );
+    my_database.delegate(
+        &Target::Variable("my_var1".to_string()),
+        &"bob".to_string(),
+        &Right::Delegate,
+        &"anyone".to_string(),
+    );
+
+    //add principals to database after anyone has some permissions
+    my_database.create_principal(&"alice".to_string(), &hash("alice_pass".to_string()));
+
+    //alice created my_var2
+    my_database.delegate(&Target::Variable("my_var2".to_string()), &"admin".to_string(), &Right::Read, &"alice".to_string());
+    my_database.delegate(&Target::Variable("my_var2".to_string()), &"admin".to_string(), &Right::Write, &"alice".to_string());
+    my_database.delegate(&Target::Variable("my_var2".to_string()), &"admin".to_string(), &Right::Append, &"alice".to_string());
+    my_database.delegate(&Target::Variable("my_var2".to_string()), &"admin".to_string(), &Right::Delegate, &"alice".to_string());
+
+    //change the default
+    my_database.set_default_delegator(&"alice".to_string());
+
+    //change bob's password
+    my_database.change_password(&"bob".to_string(), &hash("bob_new_pass".to_string()) );
+    assert_eq!(
+        my_database.check_pass(&"bob".to_string(),&hash("bob_new_pass".to_string())) &&
+            !my_database.check_pass(&"bob".to_string(),&hash("bob_pass".to_string())),
+        true
+    );
+
+    //add principals to database after new default alice has some permissions other than prev default anyone
+    my_database.create_principal(&"john".to_string(), &hash("john_pass".to_string()) );
+    // check for correct permissions
+    assert_eq!(
+        my_database.check_right(&Target::Variable("my_var2".to_string()), &Right::Read, &"john".to_string()) &&
+            my_database.check_right(&Target::Variable("my_var2".to_string()), &Right::Write, &"john".to_string()) &&
+            my_database.check_right(&Target::Variable("my_var2".to_string()), &Right::Append, &"john".to_string()) &&
+            my_database.check_right(&Target::Variable("my_var2".to_string()), &Right::Delegate, &"john".to_string()),
+        true
+    );
+
+    //alice created my_var3
+    my_database.delegate(&Target::Variable("my_var3".to_string()), &"admin".to_string(), &Right::Read, &"alice".to_string());
+    my_database.delegate(&Target::Variable("my_var3".to_string()), &"admin".to_string(), &Right::Write, &"alice".to_string());
+    my_database.delegate(&Target::Variable("my_var3".to_string()), &"admin".to_string(), &Right::Append, &"alice".to_string());
+    my_database.delegate(&Target::Variable("my_var3".to_string()), &"admin".to_string(), &Right::Delegate, &"alice".to_string());
+
+    //add principals to database after new default alice has some permissions other than prev default anyone
+    my_database.create_principal(&"git".to_string(), &hash("git_pass".to_string()) );
+    // check for in-correct permissions
+    assert_eq!(
+        my_database.check_right(&Target::Variable("my_var3".to_string()), &Right::Read, &"john".to_string()) |
+            my_database.check_right(&Target::Variable("my_var3".to_string()), &Right::Write, &"john".to_string()) |
+            my_database.check_right(&Target::Variable("my_var3".to_string()), &Right::Append, &"john".to_string()) |
+            my_database.check_right(&Target::Variable("my_var3".to_string()), &Right::Delegate, &"john".to_string()),
+        false
+    );
+
 
 
 
